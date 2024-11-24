6e279f36a82b4e2d40713909e304791ff679a47e
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2024-11-24 06:22:03 -0500

    Update property.py

--- src/models/property.py
+++ src/models/property.py
@@ -1,9 +1,7 @@
 class Property:
-    def __init__(self, light_consumption, devices, address, city, solar_panel=None):
+    def __init__(self, light_consumption, devices, solar_panel=None):
         self._light_consumption = light_consumption
         self._devices = devices
-        self._address = address
-        self._city = city
         self._solar_panel = solar_panel
 
     @property
@@ -21,22 +19,6 @@ class Property:
     @get_devices.setter
     def set_devices(self, new_device):
         self._devices = new_device
-
-    @property
-    def get_address(self):
-        self._address
-
-    @get_address.setter
-    def set_address(self, new_address):
-        self._address = new_address
-
-    @property
-    def get_city(self):
-        return self._city
-
-    @get_city.setter
-    def set_city(self, new_city):
-        self._city = new_city
 
     @property
     def get_solar_panel(self):
